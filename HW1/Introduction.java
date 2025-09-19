499f4a2a431adb11e0ab045e3041368ae2680f52
LuuQuangDung-24020085 <24020085@vnu.edu.vn>
2025-09-19 17:25:14 +0700

    Task B week 1_Information

---
+++ HW1/Introduction.java
@@ -0,0 +1,18 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Lưu Quang Dũng");
+        System.out.println("24020085");
+        System.out.println("K69I-IT4");
+        System.out.println("LuuQuangDung-24020085");
+        System.out.println("24020085@vnu.edu.vn");
+
+        int n = 9;
+        for (int i = 0; i <= 9; i++) {
+            System.out.println(n + " bottles of beer on the wall, " + n + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+            n --;
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+
+}
