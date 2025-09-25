4f4ba7a3913ad727a101a89d5ed8706063010f6c
LuuQuangDung-24020085 <24020085@vnu.edu.vn>
2025-09-25 21:57:42 +0700

    Ex 1.1

---
+++ HW1/UseThree.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class UseThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name01 = sc.next();
+        String name02 = sc.next();
+        String name03 = sc.next();
+
+        System.out.println("Hi " + name01 + ", " +  name02 + ", and " + name03  + ".");
+    }
+}
